5b68871c88578359e91eb08663255617c4f966ee
hamda66 <hamda6464@gmail.com>
2025-10-06 17:55:21 +0100

    commit NetworkSecurity changes

--- NetworkSecurity.tf
+++ NetworkSecurity.tf
@@ -18,8 +18,7 @@ resource "aws_vpc_security_group_egress_rule" "egress_rule" {
    security_group_id = aws_security_group.ec2_security.id
    from_port = 0
     to_port = 0
-    ip_protocol = "-1"
+    ip_protocol = "tcp"
     cidr_ipv4 = ["0.0.0.0/0"]
  }
 
-}
