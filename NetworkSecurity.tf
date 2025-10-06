c67d31a50f7c8ec1b00dbf95d74814f684054e6a
hamda66 <hamda6464@gmail.com>
2025-10-06 16:00:54 +0100

    NSG update

--- NetworkSecurity.tf
+++ NetworkSecurity.tf
@@ -4,20 +4,20 @@
 resource "aws_security_group" "ec2_security" {
   name = "ec2_secuirty"
   vpc_id = aws_vpc.hamda_vpc.id
+}
 
- ingress = {
+resource "aws_vpc_security_group_ingress_rule" "ingress_rule" {
+ security_group_id = aws_security_group.ec2_security.id
     from_port = 443
     to_port = 443
-    protocol = "tcp"
-    cidr_block = [aws_vpc.hamda_vpc.cidr_block]
+    ip_protocol = "tcp"
+    cidr_ipv4 = [aws_vpc.hamda_vpc.cidr_block]
+}
 
- }
-
- egress = {
-    from_port = 0
+resource "aws_vpc_security_group_egress_rule" "egress_rule" {
+   security_group_id = aws_security_group.ec2_security.id
+   from_port = 0
     to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
- }
-
+    ip_protocol = "-1"
+    cidr_ipv4 = ["0.0.0.0/0"]
 }
